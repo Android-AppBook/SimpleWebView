2eabc18ec4271e52446dade4ba6491552d700b8f
Jacob Samro <admin@jsam.in>
2017-12-11 13:11:51 +0530

    Simple WebView Project

---
+++ app/src/main/java/com/aroliant/simplewebviewapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.aroliant.simplewebviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://aroliant.com");
+
+    }
+}
